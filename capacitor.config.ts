38fed721a0f12e17aabd0248ede67891a65e3880
GeneralWand <generalwandile41@gmail.com>
2024-07-23 15:09:31 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Registrar',
+  webDir: 'www'
+};
+
+export default config;

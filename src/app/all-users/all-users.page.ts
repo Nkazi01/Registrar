59b73b5409074cc2caecf541c54e01b588493a85
Nkosi <nkosiphila901@gmail.com>
2024-07-30 16:40:09 +0200

    under construction

--- src/app/all-users/all-users.page.ts
+++ src/app/all-users/all-users.page.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-users.page.scss'],
 })
 export class AllUsersPage implements OnInit {
+  
+  users = [
+    { email: 'user1@example.com', staffNumber: '123', role: 'Admin' },
+    { email: 'user2@example.com', staffNumber: '456', role: 'User' },
+    // Add more users as needed
+  ];
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }

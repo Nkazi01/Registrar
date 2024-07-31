87255f5b8b5504c734b9e15ff758fc8b882d922d
Nkosi <nkosiphila901@gmail.com>
2024-07-31 16:19:33 +0200

    31 JULY 24 (UPDATE_PUSH) 04:20

--- src/app/all-users/all-users.page.ts
+++ src/app/all-users/all-users.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-all-users',
   templateUrl: './all-users.page.html',
